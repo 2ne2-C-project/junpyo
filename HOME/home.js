8997815442e231bdb7f5dfe498b851bad50832c8
leehyoeun <s25049@gsm.hs.kr>
2025-06-11 11:08:06 +0900

    클릭 이벤트 연결, 도전과제 추가(이미지x)

--- HOME/home.js
+++ HOME/home.js
@@ -9,27 +9,34 @@ window.onload = function() {
 };
 
 // 초기 지능값
-let intelligence = parseInt(localStorage.getItem('intelligence'))
+// let intelligence = parseInt(localStorage.getItem('intelligence'))
 
 // 랜덤 지능
-gianRandomIntelligence(0);
+// gianRandomIntelligence(0);
 
 // 자동 저장
 
-setInterval(() => {
-  localStorage.setItem('intelligence', intelligence);
-}, 3000);
+let intelligence = 0;
 
-function click() {
-  Module.ccall('click', null, [], []); // click() 호출
+function click() { // click() 호출
+  Module.ccall('click', null, [], []);
   get_intelligence();
 }
 
-function get_intelligence() {
-  const value = Module.ccall('get_intelligence', 'number', [], []); // get_intelligence() 호출
-  document.getElementById('intelligence').textContent = value;
-  }
+function get_intelligence() { // get_intelligence() 호출
+  intelligence = Module.ccall('get_intelligence', 'number', [], []);
+  document.getElementById('intelligence').textContent = intelligence;
+}
 
-document.addEventListener('click', () => {
-  click();
-});
+Module.onRuntimeInitialized = () => {
+  // 전체 화면 클릭시 실행
+  document.addEventListener('click', click);
+
+  // 주기적으로 저장
+  setInterval(() => {
+    localStorage.setItem('intelligence', intelligence);
+  }, 3000);
+
+  get_intelligence();
+};
+
