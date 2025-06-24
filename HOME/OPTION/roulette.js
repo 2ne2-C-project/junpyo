44afb37c04633785ae318313f0ede63baef9c36f
leehyoeun <s25049@gsm.hs.kr>
2025-06-24 19:24:50 +0900

    파일 옮기고 인벤토리 추가

--- HOME/OPTION/roulette.js
+++ HOME/OPTION/roulette.js
@@ -76,3 +76,18 @@ const rotate = () => {
 
 // 페이지 로드 시 돌림판 생성
 newMake();
+
+// const modal = document.querySelector('.modal');
+// const modalOpen = document.querySelector('.modal_btn');
+// const modalClose = document.querySelector('.close_btn');
+
+// //열기 버튼을 눌렀을 때 모달팝업이 열림
+// modalOpen.addEventListener('click',function(){
+// 	//'on' class 추가
+//   modal.classList.add('on');
+// });
+// //닫기 버튼을 눌렀을 때 모달팝업이 닫힘
+// modalClose.addEventListener('click',function(){
+//   //'on' class 제거
+//   modal.classList.remove('on');
+// });
